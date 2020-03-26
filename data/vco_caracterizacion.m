480badd84e03d9ce271a4a7d6b2f0143c1792bbd
rgunam <47115727+rgunam@users.noreply.github.com>
2020-03-26 11:46:00 -0600

    Create vco_caracterizacion.m

---
+++ data/vco_caracterizacion.m
@@ -0,0 +1,20 @@
+clear all
+close all
+clc
+
+D = csvread('vco.csv');
+
+%accediendo a las calumnas
+
+Num = D(:,1);  %número de (r,c) r: renglones, c: columnas
+               % (:,1) => todos los renglones (:) de la primera col (1)
+               
+
+Voltaje = D(:,2);   %segunda columna, datos de voltaje
+
+Frecuencia = D(:,3); %tecerca columna, datos de frecuencia
+
+figure
+plot(Voltaje, Frecuencia,'.','Markersize',15);
+xlabel('Voltaje [V]')
+ylabel('Frecuencia [Hz]')
